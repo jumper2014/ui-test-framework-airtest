4ece8b506ff1141db4768e11ac6ee78f64f7f0dd
zyt <ijumper@163.com>
2019-12-29 23:30:23 +0800

    enable start case

--- misc/coding/wx_send.air/wx_send.py
+++ misc/coding/wx_send.air/wx_send.py
@@ -6,25 +6,31 @@ from poco.drivers.android.uiautomation import AndroidUiautomationPoco
 
 poco = AndroidUiautomationPoco(force_restart=False)
 
-wx_tab = poco("com.tencent.mm:id/bp").offspring("android.widget.LinearLayout").offspring("com.tencent.mm:id/d7b")
+# wx_tab = poco("com.tencent.mm:id/bp").offspring("android.widget.LinearLayout").offspring("com.tencent.mm:id/d7b")
 
-search_btn = poco("android.widget.LinearLayout").offspring("com.tencent.mm:id/jd")
-search_input = poco("android.widget.LinearLayout").offspring("com.tencent.mm:id/c1").child("android.widget.RelativeLayout").offspring("com.tencent.mm:id/kh")
+# search_btn = poco("android.widget.LinearLayout").offspring("com.tencent.mm:id/jd")
+# search_input = poco("android.widget.LinearLayout").offspring("com.tencent.mm:id/c1").child("android.widget.RelativeLayout").offspring("com.tencent.mm:id/kh")
 
 
-if wx_tab:       
-    wx_tab.click()
-    sleep(1)
-if search_btn:
-    search_btn.click()
-    sleep(1)
-if search_input:
-    search_input.click()
-    sleep(1)
-    text("旺福", enter=True)
+# if wx_tab:       
+#     wx_tab.click()
+#     sleep(1)
+# if search_btn:
+#     search_btn.click()
+#     sleep(1)
+# if search_input:
+#     search_input.click()
+#     sleep(1)
+#     text("旺福", enter=True)
    
 
-poco("com.tencent.mm:id/bv").offspring("android.widget.LinearLayout").offspring("com.tencent.mm:id/tb")
+poco("com.tencent.mm:id/rb").click()
+
+
+# poco("com.tencent.mm:id/r_").click()
+
+
+
 
 
 
